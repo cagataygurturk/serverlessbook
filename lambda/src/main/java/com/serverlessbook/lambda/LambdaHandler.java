e3eb6cd00332f766832b8306f55ff5ef7cf2a811
Cagatay Gurturk <cagatay@itusozluk.com>
2016-08-22 17:51:45 +0200

    step11

--- lambda/src/main/java/com/serverlessbook/lambda/LambdaHandler.java
+++ lambda/src/main/java/com/serverlessbook/lambda/LambdaHandler.java
@@ -2,6 +2,7 @@ package com.serverlessbook.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,9 +10,14 @@ import java.io.OutputStream;
 
 public abstract class LambdaHandler<I, O> implements RequestStreamHandler {
 
+    final ObjectMapper mapper;
+
+    protected LambdaHandler() {
+        mapper = new ObjectMapper();
+    }
+
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-
     }
 
     public abstract O handleRequest(I input, Context context);
