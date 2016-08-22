ea71f3669694ba732c9b4791a991b12d3ce99a37
Cagatay Gurturk <cagatay@itusozluk.com>
2016-08-22 17:41:24 +0200

    step10

--- lambda/src/test/java/com/serverlessbook/lambda/LambdaHandlerTest.java
+++ lambda/src/test/java/com/serverlessbook/lambda/LambdaHandlerTest.java
@@ -1,6 +1,15 @@
 package com.serverlessbook.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
 
 public class LambdaHandlerTest {
 
@@ -19,5 +28,29 @@ public class LambdaHandlerTest {
             testOutput.value = input.value;
             return testOutput;
         }
+    }
+
+    @Test
+    public void handleRequest() throws Exception {
+        String jsonInputAndExpectedOutput = "{\"value\":\"testValue\"}";
+        InputStream exampleInputStream = new ByteArrayInputStream(jsonInputAndExpectedOutput.getBytes(StandardCharsets.UTF_8));
+        OutputStream exampleOutputStream = new OutputStream() {
+
+            private final StringBuilder stringBuilder = new StringBuilder();
+
+            @Override
+            public void write(int b) {
+                stringBuilder.append((char) b);
+            }
+
+            @Override
+            public String toString() {
+                return stringBuilder.toString();
+            }
+        };
+
+        TestLambdaHandler lambdaHandler = new TestLambdaHandler();
+        lambdaHandler.handleRequest(exampleInputStream, exampleOutputStream, null);
+        assertEquals(jsonInputAndExpectedOutput, exampleOutputStream.toString());
     }
 }
