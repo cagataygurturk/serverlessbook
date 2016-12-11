54d12aaa4d2cccad3a00a9c5efa6ced601311b03
cguertuerk <cguertuerk@team.mobile.de>
2016-12-11 21:07:02 +0100

    step68

---
+++ lambda-authorizer/src/main/java/com/serverlessbook/lambda/authorizer/DependencyInjectionModule.java
@@ -0,0 +1,11 @@
+package com.serverlessbook.lambda.authorizer;
+
+import com.google.inject.AbstractModule;
+
+public class DependencyInjectionModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+}
