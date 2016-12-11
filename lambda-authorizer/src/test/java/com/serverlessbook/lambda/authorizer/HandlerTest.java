7539ee307d549d834095cd92c6dff4d57b775668
cguertuerk <cguertuerk@team.mobile.de>
2016-12-11 21:54:14 +0100

    step73

---
+++ lambda-authorizer/src/test/java/com/serverlessbook/lambda/authorizer/HandlerTest.java
@@ -0,0 +1,11 @@
+package com.serverlessbook.lambda.authorizer;
+
+import org.junit.Test;
+
+public class HandlerTest {
+
+    @Test
+    public void testDependencies() throws Exception {
+        Handler testHandler = new Handler();
+    }
+}
