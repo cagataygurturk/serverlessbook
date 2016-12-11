b9053acb873e5ede746fc040cf999f74e459dd7e
cguertuerk <cguertuerk@team.mobile.de>
2016-12-11 23:10:25 +0100

    step85

--- lambda-authorizer/src/main/java/com/serverlessbook/lambda/authorizer/DependencyInjectionModule.java
+++ lambda-authorizer/src/main/java/com/serverlessbook/lambda/authorizer/DependencyInjectionModule.java
@@ -3,11 +3,14 @@ package com.serverlessbook.lambda.authorizer;
 import com.google.inject.AbstractModule;
 import com.serverlessbook.services.user.UserService;
 import com.serverlessbook.services.user.UserServiceImpl;
+import com.serverlessbook.services.user.repository.UserRepository;
+import com.serverlessbook.services.user.repository.UserRepositoryDynamoDB;
 
 public class DependencyInjectionModule extends AbstractModule {
 
     @Override
     protected void configure() {
         bind(UserService.class).to(UserServiceImpl.class);
+        bind(UserRepository.class).to(UserRepositoryDynamoDB.class);
     }
 }
