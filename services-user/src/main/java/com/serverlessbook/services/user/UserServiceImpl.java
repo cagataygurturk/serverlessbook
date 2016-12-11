6789c0dffaa8687de8f2171ce2098f4ee42553e5
cguertuerk <cguertuerk@team.mobile.de>
2016-12-11 22:50:15 +0100

    step81

--- services-user/src/main/java/com/serverlessbook/services/user/UserServiceImpl.java
+++ services-user/src/main/java/com/serverlessbook/services/user/UserServiceImpl.java
@@ -1,8 +1,18 @@
 package com.serverlessbook.services.user;
 
 import com.serverlessbook.services.user.domain.User;
+import com.serverlessbook.services.user.repository.UserRepository;
+
+import java.util.Objects;
 
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        Objects.requireNonNull(userRepository);
+    }
 
     @Override
     public User getUserByToken(String token) throws UserNotFoundException {
