132ee54cad2db6e9083d09dba86248abaf08e257
cguertuerk <cguertuerk@team.mobile.de>
2016-12-11 21:29:27 +0100

    step69

--- lambda-authorizer/src/main/java/com/serverlessbook/lambda/authorizer/DependencyInjectionModule.java
+++ lambda-authorizer/src/main/java/com/serverlessbook/lambda/authorizer/DependencyInjectionModule.java
@@ -1,11 +1,13 @@
 package com.serverlessbook.lambda.authorizer;
 
 import com.google.inject.AbstractModule;
+import com.serverlessbook.services.user.UserService;
+import com.serverlessbook.services.user.UserServiceImpl;
 
 public class DependencyInjectionModule extends AbstractModule {
 
     @Override
     protected void configure() {
-
+        bind(UserService.class).to(UserServiceImpl.class);
     }
 }
