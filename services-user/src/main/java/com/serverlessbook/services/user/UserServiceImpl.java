250e88ab9e4aeca524858542bcf6a170b40c6070
cguertuerk <cguertuerk@team.mobile.de>
2016-12-11 23:04:36 +0100

    step83

--- services-user/src/main/java/com/serverlessbook/services/user/UserServiceImpl.java
+++ services-user/src/main/java/com/serverlessbook/services/user/UserServiceImpl.java
@@ -3,12 +3,14 @@ package com.serverlessbook.services.user;
 import com.serverlessbook.services.user.domain.User;
 import com.serverlessbook.services.user.repository.UserRepository;
 
+import javax.inject.Inject;
 import java.util.Objects;
 
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+    @Inject
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
         Objects.requireNonNull(userRepository);
