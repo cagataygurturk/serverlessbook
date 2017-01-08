63128aeacca65cd5148b654491b80d9df351498c
cguertuerk <cguertuerk@team.mobile.de>
2017-01-08 18:10:04 +0100

    step93

---
+++ services-user/src/main/java/com/serverlessbook/services/user/domain/Token.java
@@ -0,0 +1,24 @@
+package com.serverlessbook.services.user.domain;
+
+public class Token {
+
+    private String token;
+
+    private String userId;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
