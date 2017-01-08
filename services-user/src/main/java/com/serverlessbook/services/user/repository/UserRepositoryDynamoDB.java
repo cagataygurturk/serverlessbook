4e70eb63bfb9e3c2b9db37e8af55b86da75b8c1e
cguertuerk <cguertuerk@team.mobile.de>
2017-01-08 21:23:02 +0100

    step98

--- services-user/src/main/java/com/serverlessbook/services/user/repository/UserRepositoryDynamoDB.java
+++ services-user/src/main/java/com/serverlessbook/services/user/repository/UserRepositoryDynamoDB.java
@@ -1,11 +1,20 @@
 package com.serverlessbook.services.user.repository;
 
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.serverlessbook.services.user.domain.User;
 
+import javax.inject.Inject;
 import java.util.Optional;
 
 public class UserRepositoryDynamoDB implements UserRepository {
+
+    private final DynamoDBMapper dynamoDBMapper;
+
+    @Inject
+    public UserRepositoryDynamoDB(DynamoDBMapper dynamoDBMapper) {
+        this.dynamoDBMapper = dynamoDBMapper;
+    }
 
     @Override
     public Optional<User> getUserByToken(String token) {
