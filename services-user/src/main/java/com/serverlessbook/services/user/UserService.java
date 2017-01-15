1b1221f44f00347d6ddcdc1cbe546dd320210120
cguertuerk <cguertuerk@team.mobile.de>
2017-01-16 00:21:08 +0100

    step116

--- services-user/src/main/java/com/serverlessbook/services/user/UserService.java
+++ services-user/src/main/java/com/serverlessbook/services/user/UserService.java
@@ -2,7 +2,10 @@ package com.serverlessbook.services.user;
 
 
 import com.serverlessbook.services.user.domain.User;
+import com.serverlessbook.services.user.exception.UserRegistrationException;
 
 public interface UserService {
     User getUserByToken(String token) throws UserNotFoundException;
+
+    User registerNewUser(String username, String email) throws UserRegistrationException;
 }
