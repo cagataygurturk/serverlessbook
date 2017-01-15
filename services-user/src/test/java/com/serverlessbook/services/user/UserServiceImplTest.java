0d1ea4a97eeebc6c2eb78a3f29613a5e5a8ab845
cguertuerk <cguertuerk@team.mobile.de>
2017-01-16 00:43:17 +0100

    step120

--- services-user/src/test/java/com/serverlessbook/services/user/UserServiceImplTest.java
+++ services-user/src/test/java/com/serverlessbook/services/user/UserServiceImplTest.java
@@ -2,9 +2,13 @@ package com.serverlessbook.services.user;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.serverlessbook.repository.DynamoDBMapperWithCustomTableName;
+import com.serverlessbook.services.user.exception.AnotherUserWithSameUsernameExistsException;
 import com.serverlessbook.services.user.repository.UserRepositoryDynamoDB;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import java.util.UUID;
 
 
 public class UserServiceImplTest {
@@ -14,6 +18,20 @@ public class UserServiceImplTest {
 
     private UserService getUserService() {
         return new UserServiceImpl(new UserRepositoryDynamoDB(new DynamoDBMapperWithCustomTableName(new AmazonDynamoDBClient())));
+    }
+
+    @Test
+    public void failedUserRegistrationWithExistingUsernameTest() throws Exception {
+
+        thrown.expect(AnotherUserWithSameUsernameExistsException.class);
+
+        UserService userService = getUserService();
+
+        final String username = UUID.randomUUID() + "test-username";
+
+        userService.registerNewUser(username, UUID.randomUUID() + "@test.com");
+        //Second call should fail
+        userService.registerNewUser(username, UUID.randomUUID() + "@test.com");
     }
 
 }
