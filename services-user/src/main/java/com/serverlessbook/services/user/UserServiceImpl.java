c30cd7e19dae426d680df5f33f38ca6c52109f2e
cguertuerk <cguertuerk@team.mobile.de>
2017-01-16 00:33:57 +0100

    step117

--- services-user/src/main/java/com/serverlessbook/services/user/UserServiceImpl.java
+++ services-user/src/main/java/com/serverlessbook/services/user/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.serverlessbook.services.user;
 
 import com.serverlessbook.services.user.domain.User;
+import com.serverlessbook.services.user.exception.UserRegistrationException;
 import com.serverlessbook.services.user.repository.UserRepository;
 
 import javax.inject.Inject;
 import java.util.Objects;
+import java.util.UUID;
 
 public class UserServiceImpl implements UserService {
 
@@ -19,6 +21,22 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUserByToken(String token) throws UserNotFoundException {
         return userRepository.getUserByToken(token).orElseThrow(UserNotFoundException::new);
+    }
+
+    @Override
+    public User registerNewUser(String username, String email) throws UserRegistrationException {
+
+        checkEmailValidity(email);
+        checkEmailUniqueness(email);
+        checkUsernameUniqueness(username);
+
+        User newUser = new User()
+                .setId(UUID.randomUUID().toString())
+                .setUsername(username)
+                .setEmail(email);
+
+        userRepository.saveUser(newUser);
+        return newUser;
     }
 
 }
