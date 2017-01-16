f01b1ebf5fbb49271100c2e01fff5924b709f643
cguertuerk <cguertuerk@team.mobile.de>
2017-01-16 01:27:26 +0100

    step129

--- services-user/src/main/java/com/serverlessbook/services/user/exception/UserRegistrationException.java
+++ services-user/src/main/java/com/serverlessbook/services/user/exception/UserRegistrationException.java
@@ -1,7 +1,7 @@
 package com.serverlessbook.services.user.exception;
 
 
-public abstract class UserRegistrationException extends Exception {
+public abstract class UserRegistrationException extends RuntimeException {
     private static final long serialVersionUID = -7628860081079461234L;
 
     protected UserRegistrationException(String message) {
