5d1330d9fe29bc844a9bf9de751ca71ee16049a5
Cagatay Gurturk <cguertuerk@team.mobile.de>
2017-05-08 01:13:04 +0200

    step168

--- lambda-userregistration-welcomemail/src/main/java/com/serverlessbook/lambda/userregistration/welcomemail/Handler.java
+++ lambda-userregistration-welcomemail/src/main/java/com/serverlessbook/lambda/userregistration/welcomemail/Handler.java
@@ -10,56 +10,68 @@ import com.amazonaws.services.simpleemail.model.Content;
 import com.amazonaws.services.simpleemail.model.Destination;
 import com.amazonaws.services.simpleemail.model.Message;
 import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+
+import java.io.IOException;
 import java.util.Objects;
 import javax.inject.Inject;
+
+import com.serverlessbook.services.user.domain.User;
 import org.apache.log4j.Logger;
 
 public class Handler implements RequestHandler<SNSEvent, Void> {
 
-  private static final Injector INJECTOR = Guice.createInjector();
+    private static final Injector INJECTOR = Guice.createInjector();
 
-  private static final Logger LOGGER = Logger.getLogger(Handler.class);
+    private static final Logger LOGGER = Logger.getLogger(Handler.class);
 
-  private AmazonSimpleEmailServiceClient simpleEmailServiceClient;
+    private AmazonSimpleEmailServiceClient simpleEmailServiceClient;
 
-  @Inject
-  public Handler setSimpleEmailServiceClient(
-      AmazonSimpleEmailServiceClient simpleEmailServiceClient) {
-    this.simpleEmailServiceClient = simpleEmailServiceClient;
-    return this;
-  }
-
-  public Handler() {
-    INJECTOR.injectMembers(this);
-    Objects.nonNull(simpleEmailServiceClient);
-  }
-
-  private void sendEmail(final String emailAddress) {
-    Destination destination = new Destination().withToAddresses(emailAddress);
-
-    Message message = new Message()
-        .withBody(new Body().withText(new Content("Welcome to our forum!")))
-        .withSubject(new Content("Welcome!"));
-
-    try {
-      LOGGER.debug("Sending welcome mail to " + emailAddress);
-      simpleEmailServiceClient.sendEmail(new SendEmailRequest()
-          .withDestination(destination)
-          .withSource(System.getenv("SenderEmail"))
-          .withMessage(message)
-      );
-      LOGGER.debug("Sending welcome mail to " + emailAddress + " succeeded");
-    } catch (Exception anyException) {
-      LOGGER.error("Sending welcome mail to " + emailAddress + " failed: ", anyException);
+    @Inject
+    public Handler setSimpleEmailServiceClient(
+            AmazonSimpleEmailServiceClient simpleEmailServiceClient) {
+        this.simpleEmailServiceClient = simpleEmailServiceClient;
+        return this;
     }
 
-  }
+    public Handler() {
+        INJECTOR.injectMembers(this);
+        Objects.nonNull(simpleEmailServiceClient);
+    }
 
-  @Override
-  public Void handleRequest(SNSEvent input, Context context) {
-    input.getRecords().forEach(snsMessage -> sendEmail(snsMessage.getSNS().getMessage()));
-    return null;
-  }
+    private void sendEmail(final User user) {
+        final String emailAddress = user.getEmail();
+        Destination destination = new Destination().withToAddresses(emailAddress);
+
+        Message message = new Message()
+                .withBody(new Body().withText(new Content("Welcome to our forum!")))
+                .withSubject(new Content("Welcome!"));
+
+        try {
+            LOGGER.debug("Sending welcome mail to " + emailAddress);
+            simpleEmailServiceClient.sendEmail(new SendEmailRequest()
+                    .withDestination(destination)
+                    .withSource(System.getenv("SenderEmail"))
+                    .withMessage(message)
+            );
+            LOGGER.debug("Sending welcome mail to " + emailAddress + " succeeded");
+        } catch (Exception anyException) {
+            LOGGER.error("Sending welcome mail to " + emailAddress + " failed: ", anyException);
+        }
+
+    }
+
+    @Override
+    public Void handleRequest(SNSEvent input, Context context) {
+        input.getRecords().forEach(snsMessage -> {
+            try {
+                sendEmail(new ObjectMapper().readValue(snsMessage.getSNS().getMessage(), User.class));
+            } catch (IOException anyException) {
+                LOGGER.error("JSON could not be deserialized", anyException);
+            }
+        });
+        return null;
+    }
 }
